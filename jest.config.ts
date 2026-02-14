3e2244cfa3b46e0b50ea298e18b25777258997eb
Allan Prado <allanp.vba@gmail.com>
2026-02-14 02:42:52 -0300

    refactor jest files

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,18 @@
-import createJestConfig from "next/jest.js";
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
 
-const config = createJestConfig({
+const createJestConfig = nextJest({
   dir: "./"
-})({
+});
+
+const config: Config = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jsdom",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1"
   },
   coveragePathIgnorePatterns: ["/node_modules/", "/.next/", "/e2e/", "/src/components/ui/", "/src/lib/"],
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/", "<rootDir>/e2e/"]
-});
+};
 
-export default config;
+export default createJestConfig(config);
