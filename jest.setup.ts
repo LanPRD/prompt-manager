c2ceea86975369e75272adc7406af711506c5f7f
Allan Prado <allanp.vba@gmail.com>
2026-03-14 17:01:16 -0300

    chore: add crypto and text encoding polyfills to jest setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,13 @@
 import "@testing-library/jest-dom";
+
+import { webcrypto } from "crypto";
+import { TextDecoder, TextEncoder } from "util";
+
+globalThis.TextEncoder = TextEncoder;
+globalThis.TextDecoder = TextDecoder;
+
+if (!globalThis.crypto) {
+  (globalThis as any).crypto = webcrypto;
+}
+
+expect.extend({});
