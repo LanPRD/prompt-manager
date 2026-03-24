9a17bf1f067d96319a2ed27eef59af953a27ceda
Allan Prado <allanp.vba@gmail.com>
2026-03-23 23:14:20 -0300

    feat: implement prompt form submission with validation and toast feedback

--- src/components/prompts/prompt-form.tsx
+++ src/components/prompts/prompt-form.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { createPromptAction } from "@/app/actions/prompt.actions";
 import { CreatePromptDto, createPromptSchema } from "@/core/application/prompts/create-prompt.dto";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { Button } from "../ui/button";
 import { Form, FormControl, FormField, FormItem } from "../ui/form";
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
 
 export function PromptForm() {
+  const router = useRouter();
+
   const form = useForm<CreatePromptDto>({
     resolver: zodResolver(createPromptSchema),
     defaultValues: {
@@ -17,9 +22,21 @@ export function PromptForm() {
     }
   });
 
+  async function submit(data: CreatePromptDto) {
+    const result = await createPromptAction(data);
+
+    if (!result.success) {
+      toast.error(result.message || "Ocorreu um erro ao criar o prompt.");
+      return;
+    }
+
+    toast.success(result.message);
+    router.refresh();
+  }
+
   return (
     <Form {...form}>
-      <form action="" className="space-y-6">
+      <form onSubmit={form.handleSubmit(submit)} className="space-y-6">
         <header className="flex flex-wrap gap-2 items-center mb-6 justify-end">
           <Button type="submit" size="sm">
             Salvar
