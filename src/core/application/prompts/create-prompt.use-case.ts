57916cdce10972310cc94b49194d9a78942bc694
Allan Prado <allanp.vba@gmail.com>
2026-03-23 23:13:47 -0300

    feat: add create prompt use case

---
+++ src/core/application/prompts/create-prompt.use-case.ts
@@ -0,0 +1,16 @@
+import type { PromptRepository } from "@/core/domain/prompts/prompt.repository";
+import type { CreatePromptDto } from "./create-prompt.dto";
+
+export class CreatePromptUseCase {
+  constructor(private promptRepository: PromptRepository) {}
+
+  async execute(data: CreatePromptDto): Promise<void> {
+    const promptExists = await this.promptRepository.findByTitle(data.title);
+
+    if (promptExists) {
+      throw new Error("PROMPT_ALREADY_EXISTS");
+    }
+
+    await this.promptRepository.create(data);
+  }
+}
