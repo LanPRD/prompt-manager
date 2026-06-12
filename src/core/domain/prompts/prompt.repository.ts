56dc1945e4b77b5b4b56ea1bd05fd9defff796f0
Allan Prado <allanp.vba@gmail.com>
2026-06-11 23:58:57 -0300

    feat(domain): add delete method to PromptRepository interface

--- src/core/domain/prompts/prompt.repository.ts
+++ src/core/domain/prompts/prompt.repository.ts
@@ -4,6 +4,7 @@ import { Prompt } from "./prompt.entity";
 export interface PromptRepository {
   create(data: CreatePromptDto): Promise<void>;
   update(id: string, data: Partial<CreatePromptDto>): Promise<Prompt>;
+  delete(id: string): Promise<void>;
   findMany(): Promise<Prompt[]>;
   findById(id: string): Promise<Prompt | null>;
   findByTitle(title: string): Promise<Prompt | null>;
